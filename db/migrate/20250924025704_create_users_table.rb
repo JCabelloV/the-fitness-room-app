d8cd6eca52d1937e14390ec5513ab4c00b7d38ff
JuanCabelloVargas <jicabello@miuandes.cl>
2025-09-24 00:17:22 -0300

    feat: Create User basic model andmigration with views index and show

---
+++ db/migrate/20250924025704_create_users_table.rb
@@ -0,0 +1,14 @@
+class CreateUsersTable < ActiveRecord::Migration[8.0]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :rut
+      t.string :password_digest
+      t.string :email
+      t.string :username
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+felipe_cabello = User.create!(first_name: "Felipe", last_name: "Cabello", username: "satanas", email: "cabellovargasf@gmail.com", password_digest: "1234")
+sher_roig = User.create!(first_name: "Sheryne", last_name: "Roig", username: "sherpersonaltrainer", email: "test@test.com", password_digest: "1234")
+juan_cabello  = User.create!(first_name: "Juan", last_name: "Cabello", username: "wantan", email: "Cabellovargasj@gmail.com", password_digest: "4321")
