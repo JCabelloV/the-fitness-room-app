0502f33ffaecc8b3f77e4558eb1b799cc3bd9648
JuanCabelloVargas <jicabello@miuandes.cl>
2025-09-24 09:45:28 -0300

    feat: Add Create Crud action to controller, model, view and routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,24 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    # @user = User.new({
+    # first_name: params[:user][:first_name],
+    # last_name: params[:user][:last_name],
+    # email: params[:user][:email]
+    # })
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def user_params
+    params
+      .require(:user)
+      .permit(:first_name, :last_name, :email)
+  end
 end
