df041307bfba3a2e82c01074f1a1f2e2d380d857
JuanCabelloVargas <jicabello@miuandes.cl>
2025-09-24 12:05:39 -0300

    feat: Add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: { scope: :email, message: "Este correo ya esta en uso" }
 
   def full_name
     "#{first_name} #{last_name}"
