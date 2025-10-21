a147e7d15aa7bddb717582a7a2cb692a8a9b5437
JuanCabelloVargas <jicabello@miuandes.cl>
2025-10-21 16:04:51 -0300

    feat: Create model for schedules

---
+++ app/models/schedule.rb
@@ -0,0 +1,20 @@
+class Schedule < ApplicationRecord
+  belongs_to :user
+  belongs_to :trainer, class_name: "User"
+
+  enum :status, {
+    pending: 0,
+    confirmed: 1,
+    cancelled: 2,
+    completed: 3
+  }
+
+  validates :starts_at, :ends_at, presence: true
+
+  priavte
+
+  def ends_after_starts
+    return if starts_at.blank? || ends_at.blank?
+    errors.add(:ends_at, "Debe ser mayor que la fecha de inicio") if ends_at <= starts_at
+  end
+end
